aaf1600f9f1a1de9138fff234147bcc208b5722e
Churtified <88999504+Churtified@users.noreply.github.com>
2022-09-19 12:51:00 +0930

    initial commit

--- src/makeDOM.js
+++
@@ -1,9 +0,0 @@
-export const DOM = () => {
-    const container = document.querySelector(".container");
-
-    const createHeader = (() => {
-        const header = document.createElement('header');
-        header.classList.add('header');
-        header.innerHTML =
-    })
-}
